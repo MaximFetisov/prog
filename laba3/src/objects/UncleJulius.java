1d1ad630b122fbab822426bd86a5b4e8f8bc5a7a
MaximFetisov <159959648+MaximFetisov@users.noreply.github.com>
2025-12-17 22:44:12 +0300

    Add files via upload

---
+++ laba3/src/objects/UncleJulius.java
@@ -0,0 +1,17 @@
+package objects;
+
+import enums.*;
+import interfaces.*;
+
+public class UncleJulius extends Person {
+    public UncleJulius(String name, Mood mood) {
+        super(name, mood);
+    }
+
+
+
+    @Override
+    public void speak() {
+        System.out.println(name + " говорит: \"Что за шум был ночью?\"");
+    }
+}
